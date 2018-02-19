750cdc3fe7d4e19afd63929fa8501e727775dda7
Jan Tuitman <jan_tuitman@hotmail.com>
2018-02-19 14:23:02 +0100

    initial commit

---
+++ Coleman-1.1/examples/pss.m
@@ -0,0 +1,644 @@
+/////////////////////////////////////////////////////////////
+// Examples from https://arxiv.org/abs/math/0508174        //
+// "Twists of X(7) and primitive solutions to x^2+y^3=z^7" //
+/////////////////////////////////////////////////////////////
+
+// Effective Chabauty (alone) works for C1,C2,C3,C8,C9,C10.
+
+/////////
+// C1: //
+/////////
+
+load "coleman.m";
+Q:=6*x^3*y+y^3+x;
+p:=5;
+N:=15;
+data:=coleman_data(Q,p,N);
+Qpoints:=Q_points(data,10^4);
+L,v:=effective_chabauty(data:Qpoints:=Qpoints,e:=25);
+
+L;
+Qpoints;
+v;
+
+/*
+> L;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^4),
+        b := [ 1 + O(5^15), O(5^15), O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^8),
+        b := [ 1 + O(5^15), O(5^4), -6 + O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^9),
+        b := [ 1 + O(5^15), O(5^3), O(5^6) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -312 + O(5^4),
+        b := [ 1 + O(5^15), 1562 + O(5^5), -781 + O(5^5) ],
+        inf := false>
+]
+> 
+> Qpoints;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0 ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -15258789062 + O(5^15),
+        b := [ 1 + O(5^15), 15258789062 + O(5^15), -7629394531 + O(5^15) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0 ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, -6 + O(5^15) ],
+        inf := true>
+]
+> 
+> v;
+[
+    [ 1 + O(5^5), O(5^5), -868 + O(5^5) ],
+    [ O(5^5), 1 + O(5^5), 16 + O(5^5) ]
+]
+*/
+
+/////////
+// C2: //
+/////////
+
+load "coleman.m";
+Q:=3*x^3*y+y^3+2*x;
+p:=5;
+N:=15;
+data:=coleman_data(Q,p,N);
+Qpoints:=Q_points(data,10^4);
+L,v:=effective_chabauty(data:Qpoints:=Qpoints,e:=25);
+
+L;
+Qpoints;
+v;
+
+/*
+> L;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^4),
+        b := [ 1 + O(5^15), O(5^15), O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^8),
+        b := [ 1 + O(5^15), O(5^4), -3 + O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^9),
+        b := [ 1 + O(5^15), O(5^3), O(5^6) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -1 + O(5^8),
+        b := [ 1 + O(5^15), -1 + O(5^4), 1 + O(5^4) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -1 + O(5^4),
+        b := [ 1 + O(5^15), 2 + O(5^5), 4 + O(5^5) ],
+        inf := false>
+]
+> 
+> Qpoints;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0 ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -1 + O(5^15),
+        b := [ 1 + O(5^15), 2 + O(5^15), 4 + O(5^15) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -1 + O(5^15),
+        b := [ 1 + O(5^15), -1 + O(5^15), 1 + O(5^15) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0 ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, -3 + O(5^15) ],
+        inf := true>
+]
+> 
+> v;
+[
+    [ 1 + O(5^5), O(5^5), -1303 + O(5^5) ],
+    [ O(5^5), 1 + O(5^5), -42*5^2 + O(5^5) ]
+]
+> 
+*/
+
+
+////////
+// C3 //
+////////
+
+load "coleman.m";
+Q:=y^3 + 2*x^3*y + 3*x; // take x:=1, y:=x, z:=y. 
+p:=5;
+N:=15;
+data:=coleman_data(Q,p,N);
+
+Qpoints:=Q_points(data,10^4);
+L,v:=effective_chabauty(data:Qpoints:=Qpoints,e:=25);
+
+L;
+Qpoints;
+v;
+
+/*
+> L;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^4),
+        b := [ 1 + O(5^15), O(5^15), O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^8),
+        b := [ 1 + O(5^15), O(5^4), -2 + O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^9),
+        b := [ 1 + O(5^15), O(5^3), O(5^6) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 1 + O(5^5),
+        b := [ 1 + O(5^15), -1 + O(5^4), 1 + O(5^4) ],
+        inf := false>
+]
+> Qpoints;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0 ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 1 + O(5^15),
+        b := [ 1 + O(5^15), -1 + O(5^15), 1 + O(5^15) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0 ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, -2 + O(5^15) ],
+        inf := true>
+]
+> v;
+[
+    [ 1 + O(5^5), O(5^5), 401 + O(5^5) ],
+    [ O(5^5), 1 + O(5^5), -1313 + O(5^5) ]
+]
+*/
+
+
+////////
+// C4 //
+////////
+
+load "coleman.m";
+Q:=-(7*x^3*y+3*x^2-3*x*y^2+y-y^4); // take x:=x, y:=1, z:=y and multiply by -1. 
+p:=5;
+N:=15;
+data:=coleman_data(Q,p,N);
+
+Qpoints:=Q_points(data,10^4);
+L,v:=effective_chabauty(data:Qpoints:=Qpoints,e:=25);
+
+L;
+Qpoints;
+v;
+
+/*
+> L;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^13),
+        b := [ 1 + O(5^15), O(5^15), O(5^15), O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -67073887*5 + O(5^13),
+        b := [ 1 + O(5^15), 206040388 + O(5^13), -114371956 + O(5^13), -491340178 + O(5^13) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^13),
+        b := [ 1 + O(5^15), O(5^26), O(5^52), O(5^78) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^13),
+        b := [ 1 + O(5^15), 1 + O(5^13), 1 + O(5^13), 1 + O(5^13) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -335919187 + O(5^13),
+        b := [ 1 + O(5^15), -56720952 + O(5^13), -449916821 + O(5^13), -285425783 + O(5^13) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 605718033 + O(5^13),
+        b := [ 1 + O(5^15), -306498548 + O(5^13), 62827054 + O(5^13), 541069908 + O(5^13) ],
+        inf := false>
+]
+> Qpoints;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 1 + O(5^15), 1 + O(5^15), 1 + O(5^15) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0, 0 ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0, 0 ],
+        inf := true>
+]
+> v;
+[
+    [ 1 + O(5^14), 2394632966 + O(5^14), O(5^14) ]
+]
+*/
+
+// 3 points left to rule out, same or worse for other primes, MW sieving?
+
+
+////////
+// C6 //
+////////
+
+load "coleman.m";
+Q:=y^4+2*y^3*x+3*y^2*x^2+2*y*x^3-9*y*x^2+3*y*x-y+3*x^3-y; // take x:=y, y:=x, z:=1.
+p:=5;
+N:=15;
+data:=coleman_data(Q,p,N);
+
+Qpoints:=Q_points(data,10^4);
+L,v:=effective_chabauty(data:Qpoints:=Qpoints,e:=25);
+
+L;
+Qpoints;
+v;
+
+/*
+> L;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^13),
+        b := [ 1 + O(5^15), O(5^13), O(5^15), O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^13),
+        b := [ 1 + O(5^15), -1 + O(5^13), 1 + O(5^13), -1 + O(5^13) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^13),
+        b := [ 1 + O(5^15), O(5^39), O(5^78), O(5^117) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 102352389*5 + O(5^13),
+        b := [ 1 + O(5^15), 256555568 + O(5^13), 194458874 + O(5^13), 35772932 + O(5^13) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 483340367 + O(5^13),
+        b := [ 1 + O(5^15), 285439741 + O(5^13), -205118544 + O(5^13), 301342896 + O(5^13) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 415363618 + O(5^13),
+        b := [ 1 + O(5^15), -513577579 + O(5^13), -280139384 + O(5^13), -267494289 + O(5^13) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 572843061 + O(5^13),
+        b := [ 1 + O(5^15), 501166792 + O(5^13), 327399389 + O(5^13), -172266162 + O(5^13) ],
+        inf := false>
+]
+> Qpoints;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0, 0 ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0, 0 ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), -1 + O(5^15), 1 + O(5^15), -1 + O(5^15) ],
+        inf := true>
+]
+> v;
+[
+    [ 1 + O(5^14), -1589967487 + O(5^14), 630135344 + O(5^14) ]
+]
+*/
+
+// 4 points left to rule out, same or worse for other primes, MW sieving?
+
+
+////////
+// C7 //
+////////
+
+/*
+R<x,y,z>:=PolynomialRing(RationalField(),3);
+f:=-3*x^4-6*x^3*z+6*x^2*y^2-6*x^2*y*z+15*x^2*z^2-4*x*y^3-6*x*y*z^2-4*x*z^3+6*y^\
+2*z^2-6*y*z^3;
+-3^3*Evaluate(f,[y/3,x,1]);
+36*x^3*y - 18*x^2*y^2 - 162*x^2 + 18*x*y^2 + 54*x*y + 162*x + y^4 + 6*y^3 - 45*y^2 + 36*y
+*/
+
+load "coleman.m";
+Q:=36*x^3*y - 18*x^2*y^2 - 162*x^2 + 18*x*y^2 + 54*x*y + 162*x + y^4 + 6*y^3 - 45*y^2 + 36*y;
+p:=11;
+N:=15;
+data:=coleman_data(Q,p,N);
+
+Qpoints:=Q_points(data,10^4);
+L,v:=effective_chabauty(data:Qpoints:=Qpoints,e:=50);
+
+L;
+Qpoints;
+v;
+
+/*
+> L;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(11^10),
+        b := [ 1 + O(11^15), O(11^15), O(11^15), O(11^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 857596757*11 + O(11^10),
+        b := [ 1 + O(11^15), -1721851564 + O(11^10), 2543821670 + O(11^10), -4056664907 + O(11^10) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(11^10),
+        b := [ 1 + O(11^15), O(11^10), O(11^20), O(11^30) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -890867608*11 + O(11^10),
+        b := [ 1 + O(11^15), -12883474068 + O(11^10), 10290984988 + O(11^10), 8814526617 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 41457701*11 + O(11^10),
+        b := [ 1 + O(11^15), 11379647582 + O(11^10), 9783899128 + O(11^10), 5822523681 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 663467820*11 + O(11^10),
+        b := [ 1 + O(11^15), -3771967483 + O(11^10), -8378968429 + O(11^10), 11495433067 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -9752088465 + O(11^10),
+        b := [ 1 + O(11^15), -3573456770 + O(11^10), 1478249800 + O(11^10), -3954791432 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 336543407 + O(11^10),
+        b := [ 1 + O(11^15), 41997873680 + O(11^11), 131304022998 + O(11^11), -58924492167 + O(11^11) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 2700601362 + O(11^10),
+        b := [ 1 + O(11^15), -3984588515 + O(11^10), -10955850760 + O(11^10), -8005063755 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 919007743 + O(11^10),
+        b := [ 1 + O(11^15), 12691651024 + O(11^10), -4901888642 + O(11^10), 2733701372 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -998087069 + O(11^10),
+        b := [ 1 + O(11^15), 1062249109 + O(11^10), 9079508211 + O(11^10), 9340330882 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -3167441587 + O(11^10),
+        b := [ 1 + O(11^15), -1301782747 + O(11^10), 6316512566 + O(11^10), 9438957923 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -5907354369 + O(11^10),
+        b := [ 1 + O(11^15), -1483592980 + O(11^10), -9465555934 + O(11^10), 482798832 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 8336159487 + O(11^10),
+        b := [ 1 + O(11^15), -6660595561 + O(11^10), 2946474668 + O(11^10), -3505099398 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 9467011700 + O(11^10),
+        b := [ 1 + O(11^15), -11919575782 + O(11^10), 4180389427 + O(11^10), -2655820946 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := -1142312055 + O(11^10),
+        b := [ 1 + O(11^15), -4332428470 + O(11^10), 1080600196 + O(11^10), -5306342285 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 1 + O(11^10),
+        b := [ 1 + O(11^15), O(11^10), O(11^20), O(11^30) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 4114582671 + O(11^10),
+        b := [ 1 + O(11^15), 227338924 + O(11^10), -3706748627 + O(11^10), 5940119932 + O(11^10) ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 3364916160 + O(11^10),
+        b := [ 1 + O(11^15), -36103972944 + O(11^11), 124642451134 + O(11^11), 89638763526 + O(11^11) ],
+        inf := false>
+]
+> Qpoints;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(11^15), 0, 0, 0 ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 1 + O(11^15),
+        b := [ 1 + O(11^15), 0, 0, 0 ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(11^15), 0, 0, 0 ],
+        inf := true>
+]
+> v;
+[
+    [ 1 + O(11^11), 140494362639 + O(11^11), 137574761663 + O(11^11) ]
+]
+*/
+
+// 16 points to rule out, same or worse for other primes :( 
+
+
+////////
+// C8 //
+////////
+
+/*
+R<x,y,z>:=PolynomialRing(RationalField(),3);
+f:=2*x^4-x^3*y-12*x^2*y^2+3*x^2*z^2-5*x*y^3-6*x*y^2*z+2*x*z^3-2*y^4+6*y^3*z+3*\
+y^2*z^2+2*y*z^3;
+-2^3*Evaluate(f,[x,y/2,1]);
+-16*x^4 + 4*x^3*y + 24*x^2*y^2 - 24*x^2 + 5*x*y^3 + 12*x*y^2 - 16*x + y^4 - 6*y^3 - 6*y^2 - 8*y
+*/
+
+load "coleman.m";
+Q:=-16*x^4 + 4*x^3*y + 24*x^2*y^2 - 24*x^2 + 5*x*y^3 + 12*x*y^2 - 16*x + y^4 - 6*y^3 - 6*y^2 - 8*y;
+p:=5;
+N:=15;
+data:=coleman_data(Q,p,N);
+
+Qpoints:=Q_points(data,10^4);
+L,v:=effective_chabauty(data:Qpoints:=Qpoints,e:=50);
+
+L;
+Qpoints;
+v;
+
+/*
+> L;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^13),
+        b := [ 1 + O(5^15), -1 + O(5^13), 1 + O(5^13), -1 + O(5^13) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^13),
+        b := [ 1 + O(5^15), O(5^13), O(5^26), O(5^39) ],
+        inf := false>
+]
+> Qpoints;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0, 0 ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), -1 + O(5^15), 1 + O(5^15), -1 + O(5^15) ],
+        inf := true>
+]
+> v;
+[
+    [ 1 + O(5^14), O(5^14), 2721260046 + O(5^14) ],
+    [ O(5^14), 1 + O(5^14), 2968288606 + O(5^14) ]
+]
+*/
+
+
+////////
+// C9 //
+////////
+
+/*
+R<x,y,z>:=PolynomialRing(RationalField(),3);
+f:=2*x^4+4*x^3*y-4*x^3*z-3*x^2*y^2-6*x^2*y*z+6*x^2*z^2-x*y^3-6*x*y*z^2-2*y^4+2*y^3*z-3*y^2*z^2+6*y*z^3;
+(-2)^3*Evaluate(f,[x,y/(-2),1]);
+-16*x^4 + 16*x^3*y + 32*x^3 + 6*x^2*y^2 - 24*x^2*y - 48*x^2 - x*y^3 - 24*x*y + y^4 + 2*y^3 + 6*y^2 + 24*y
+*/
+
+load "coleman.m";
+Q:=-16*x^4 + 16*x^3*y + 32*x^3 + 6*x^2*y^2 - 24*x^2*y - 48*x^2 - x*y^3 - 24*x*y + y^4 + 2*y^3 + 6*y^2 + 24*y;
+p:=5;
+N:=15;
+data:=coleman_data(Q,p,N);
+
+Qpoints:=Q_points(data,10^4);
+L,v:=effective_chabauty(data:Qpoints:=Qpoints,e:=50);
+
+L;
+Qpoints;
+v;
+
+/*
+> L;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^12),
+        b := [ 1 + O(5^15), -2 + O(5^12), 4 + O(5^12), -8 + O(5^12) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^13),
+        b := [ 1 + O(5^15), O(5^26), O(5^52), O(5^78) ],
+        inf := false>
+]
+> Qpoints;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0, 0 ],
+        inf := false>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), -2 + O(5^15), 4 + O(5^15), -8 + O(5^15) ],
+        inf := true>
+]
+> v;
+[
+    [ 1 + O(5^14), O(5^14), -1210706056 + O(5^14) ],
+    [ O(5^14), 1 + O(5^14), -271625988*5 + O(5^14) ]
+]
+*/
+
+
+/////////
+// C10 //
+/////////
+
+/*
+R<x,y,z>:=PolynomialRing(RationalField(),3);
+f:=x^3*y-x^3*z+3*x^2*z^2+3*x*y^2*z+3*x*y*z^2+3*x*z^3-y^4+y^3*z+3*y^2*z^2-12*y*z^3+3*z^4;
+-Evaluate(f,[x,y,1]);
+-x^3*y + x^3 - 3*x^2 - 3*x*y^2 - 3*x*y - 3*x + y^4 - y^3 - 3*y^2 + 12*y - 3
+*/
+
+load "coleman.m";
+Q:=-x^3*y + x^3 - 3*x^2 - 3*x*y^2 - 3*x*y - 3*x + y^4 - y^3 - 3*y^2 + 12*y - 3;
+p:=5;
+N:=15;
+data:=coleman_data(Q,p,N);
+
+Qpoints:=Q_points(data,10^4);
+L,v:=effective_chabauty(data:Qpoints:=Qpoints,e:=50);
+
+/*
+L;
+Qpoints;
+v;
+
+> L;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^8),
+        b := [ 1 + O(5^15), O(5^8), O(5^15), O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := O(5^9),
+        b := [ 1 + O(5^15), 1 + O(5^9), 1 + O(5^9), 1 + O(5^9) ],
+        inf := true>
+]
+> Qpoints;
+[
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 1 + O(5^15), 1 + O(5^15), 1 + O(5^15) ],
+        inf := true>,
+    rec<recformat<x, b, inf, xt, bt, index> | 
+        x := 0,
+        b := [ 1 + O(5^15), 0, 0, 0 ],
+        inf := true>
+]
+> v;
+[
+    [ 1 + O(5^12), O(5^12), -23017321 + O(5^12) ],
+    [ O(5^12), 1 + O(5^12), -104950818 + O(5^12) ]
+]
+*/
---
+++ example.m
@@ -0,0 +1,40 @@
+load "frob.m";
+
+Q:=y^4 + 5*x^4 - 6*x^2*y^2 + 6*x^3 + 26*x^2*y + 10*x*y^2 - 10*y^3 - 32*x^2 -40*x*y + 24*y^2 + 32*x - 16*y; // equation of the curve
+p:=17;    // prime number p
+N:=20;    // initial p-adic precision
+b:=[0,0]; // base point
+
+r,Delta,s:=auxpolys(Q);
+
+// put in a basis omega[i]dx/(dQ/dy) for H^1(X) by hand:
+
+omega:=[Zxy|];
+omega[1]:=3;
+omega[2]:=3*x;
+omega[3]:=3*y;
+omega[4]:=-160*x^4+736*x^3-16*x^2*y+436*x^2-440*x*y+68*y^2;
+omega[5]:=-80*x^3+132*x^2-40*x*y+68*y^2-96;
+omega[6]:=-48*x^2*y+84*x^2+216*x*y-12*y^2-160*x+272;
+
+denom:=3; // the omega's have to be integral, but a denominator can be specified to make the basis symplectic
+
+// first correspondence:
+
+// Z1:=hecke_corr(Q,11,10,omega,denom);
+Z1:=Matrix(RationalField(),6,6,[[0,-976,-1104,10,-6,18],[976,0,-816,-3,1,3],[1104,816,0,-3,3,-11],[-10,3,3,0,0,0],[6,-1,-3,0,0,0],[-18,-3,11,0,0,0]]);
+eta1:=-(132*x^2+148*x*y+24*y^2);
+
+Phi1:=frob_struc(Q,p,N,Z1,eta1,b,omega,denom);
+pt:=[0,0];
+Phi1_at_pt:=eval_mat_R(Phi1,pt,r);
+
+// second correspondence:
+
+// Z2:=hecke_corr(Q,7,10,omega,denom);
+Z2:=Matrix(RationalField(),6,6,[[0,112,-656,-6,6,6],[-112,0,-2576,15,9,27],[656,2576,0,3,3,-3],[6,-15,-3,0,0,0],[-6,-9,-3,0,0,0],[-6,-27,3,0,0,0]]);
+eta2:=3*(-40*x^2+148*x*y+36*y^2);
+
+Phi2:=frob_struc(Q,p,N,Z2,eta2,b,omega,denom);
+pt:=[0,0];
+Phi2_at_pt:=eval_mat_R(Phi2,pt,r);
